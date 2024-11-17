8572d8019deb9d4bd7ef35bc14a5e58f22744e46
ibiscum <ulf.luecking@gmail.com>
2024-11-17 11:01:17 +0100

    test

--- Chapter04/facade/facade_test.go
+++ Chapter04/facade/facade_test.go
@@ -2,61 +2,61 @@ package openWeatherMap
 
 import (
 	"bytes"
-	"flag"
-	"fmt"
 	"io"
 	"testing"
 )
 
-var integration = flag.Bool("integration", false, "Use to run integration tests or not")
-var apiKey = flag.String("apikey", "", "Pass your open weather map api key")
+// var integration = flag.Bool("integration", false, "Use to run integration tests or not")
+// var apiKey = flag.String("apikey", "", "Pass your open weather map api key")
 
-func init() {
-	flag.Parse()
-}
+// func init() {
+// 	flag.Parse()
+// }
 
 func Test_Integration_OpenWeatherMap_GetWeatherByCityName(t *testing.T) {
-	if !*integration {
-		t.SkipNow()
-	}
+	// if !*integration {
+	// 	t.Skip()
+	// }
+	t.Skip()
 
-	if *apiKey == "" {
-		t.Error("Not api key passed")
-	}
+	// if *apiKey == "" {
+	// 	t.Error("Not api key passed")
+	// }
 
-	weatherMap := CurrentWeatherData{*apiKey}
+	// weatherMap := CurrentWeatherData{*apiKey}
 
-	weather, err := weatherMap.GetByCityAndCountryCode("Madrid", "ES")
-	if err != nil {
-		t.Fatal(err)
-	}
+	// weather, err := weatherMap.GetByCityAndCountryCode("Madrid", "ES")
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
 
-	if weather.Coord.Lon != -3.7 {
-		t.Errorf("Lon was not -3.7 as expected. Lon=%f", weather.Coord.Lon)
-	}
+	// if weather.Coord.Lon != -3.7 {
+	// 	t.Errorf("Lon was not -3.7 as expected. Lon=%f", weather.Coord.Lon)
+	// }
 
-	fmt.Printf("Temperature in Madrid is %f celsius\n", weather.Main.Temp-273.15)
+	// fmt.Printf("Temperature in Madrid is %f celsius\n", weather.Main.Temp-273.15)
 }
 
 func Test_Integration_OpenWeatherMap_GetWeatherByGeographicalCoordinates(t *testing.T) {
-	if !*integration {
-		t.SkipNow()
-	}
+	// if !*integration {
+	// 	t.SkipNow()
+	// }
+	t.Skip()
 
-	if *apiKey == "" {
-		t.Error("Not api key passed")
-	}
+	// if *apiKey == "" {
+	// 	t.Error("Not api key passed")
+	// }
 
-	weatherMap := CurrentWeatherData{*apiKey}
+	// weatherMap := CurrentWeatherData{*apiKey}
 
-	weather, err := weatherMap.GetByGeoCoordinates(-3.7, 40.42)
-	if err != nil {
-		t.Fatal(err)
-	}
+	// weather, err := weatherMap.GetByGeoCoordinates(-3.7, 40.42)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
 
-	if weather.Cod != 200 {
-		t.Errorf("Cod was not 200 as expected. Code: %d\n", weather.Cod)
-	}
+	// if weather.Cod != 200 {
+	// 	t.Errorf("Cod was not 200 as expected. Code: %d\n", weather.Cod)
+	// }
 }
 
 func getMockData() io.Reader {
